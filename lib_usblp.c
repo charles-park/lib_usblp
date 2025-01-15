e92ff7261bc45db5fe36744d65b4e0b95427fb83
charles-park <charles.park@hardkernel.com>
2025-01-15 08:29:19 +0000

    def protocol EPL

--- lib_usblp.c
+++ lib_usblp.c
@@ -47,35 +47,17 @@ const char *EPL_FORM_START = "I8,0,001\nQ78,16\nq240\nrN\nS4\nD15\nZB\nJF\nO\nR3
     "< forum.odroid.com" or "forum.odroid.com >"
     00:1E:06:xx:xx:xx
 */
-const char *EPL_FORM_MAC_L = "A10,0,0,2,1,1,N,\"< forum.odroid.com\"\nA16,32,0,2,1,1,N,\"%s\"\n";
-const char *EPL_FORM_MAC_R = "A10,0,0,2,1,1,N,\"forum.odroid.com >\"\nA16,32,0,2,1,1,N,\"%s\"\n";
+const char *EPL_FORM_MAC_L = "A%d,0,0,2,1,1,N,\"< forum.odroid.com\"\nA%d,32,0,2,1,1,N,\"%s\"\n";
+const char *EPL_FORM_MAC_R = "A%d,0,0,2,1,1,N,\"forum.odroid.com >\"\nA%d,32,0,2,1,1,N,\"%s\"\n";
 
 /* %d = 20, 40, 60, (11 * i) * 2 */
-const char *EPL_FORM_ERR   = "A4,%d,0,2,1,1,N,\"%s\"\n";
+const char *EPL_FORM_ERR   = "A%d,%d,0,2,1,1,N,\"%c%s\"\n";
 
-const char *EPL_FORM_ERR_1 = "A4,00,0,2,1,1,N,\"%s\"\n";
-const char *EPL_FORM_ERR_2 = "A4,22,0,2,1,1,N,\"%s\"\n";
-const char *EPL_FORM_ERR_3 = "A4,44,0,2,1,1,N,\"%s\"\n";
+const char *EPL_FORM_ERR_1 = "A%d,00,0,2,1,1,N,\"%c%s\"\n";
+const char *EPL_FORM_ERR_2 = "A%d,22,0,2,1,1,N,\"%c%s\"\n";
+const char *EPL_FORM_ERR_3 = "A%d,44,0,2,1,1,N,\"%c%s\"\n";
 /* Page end */
 const char *EPL_FORM_END   = "P1\n";
-
-const char USBLP_EPL_FORM[][TEXT_WIDTH] = {
-    "I8,0,001\n",
-    "Q78,16\n",
-    "q240\n",
-    "rN\n",
-    "S4\n",
-    "D15\n",
-    "ZB\n",
-    "JF\n",
-    "O\n",
-    "R304,10\n",
-    "f100\n",
-    "N\n",
-    "A10,0,0,2,1,1,N,\"EPL Printer Test\"\n",
-    "A16,32,0,2,1,1,N,\"00:1E:06:xx:xx:xx\"\n",
-    "P1\n"
-};
 
 //------------------------------------------------------------------------------
 const char *ZPL_FORM_START = "^XA\n^CFC\n^LH0,0\n";
@@ -97,38 +79,16 @@ const char *ZPL_FORM_ERR_3 = "^FO304,60\n^FD%c%s^FS\n";
 /* Page end */
 const char *ZPL_FORM_END   = "^XZ\n";
 
-const char USBLP_ZPL_FORM[][TEXT_WIDTH] = {
-    "I8,0,001\n",
-    "Q78,16\n",
-    "q240\n",
-    "rN\n",
-    "S4\n",
-    "D15\n",
-    "ZB\n",
-    "JF\n",
-    "O\n",
-    "R304,10\n",
-    "f100\n",
-    "N\n",
-    "A10,0,0,2,1,1,N,\"ZPL Printer Test\"\n",
-    "A16,32,0,2,1,1,N,\"00:1E:06:xx:xx:xx\"\n",
-    "P1\n"
-};
+const char *ZPL_USB_INIT   = "^XA^JUF^XZ\n";
 
-#if 0
-const int8_t USBLP_ZPL_INIT[TEXT_WIDTH] = {
-    "^XA^JUF^XZ\n"
-};
+//------------------------------------------------------------------------------
+// EPL Protocol Pos
+// MAC ADDR : [A0,0 A6,0 (ZD230D)], [A6,0, A16,0 (GC420D)]
+// ERR MSG  : [A0,0 A0,22 A0,44 (ZD230D)], [A4,0 A4,22 A4,44 (GC420D)]
+//------------------------------------------------------------------------------
+#define USE_PROTOCOL_EPL
 
-const int8_t USBLP_ZPL_FORM[][TEXT_WIDTH] = {
-    "^XA\n",
-    "^CFC\n",
-    "^LH0,0\n",
-    "^FO310,25^FDZPL Printer Test^FS\n",
-    "^FO316,55^FD00:1E:06:xx:xx:xx^FS\n",
-    "^XZ\n"
-};
-#endif
+static int NLP_MODEL = 0;   /* 0 : GC420D, 1 : ZD230D */
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -259,13 +219,21 @@ static int confirm_usblp_device (char *lpname)
 int usblp_print_mac (char *msg, int ch)
 {
     FILE *fp = fopen ("usblp_mac.txt", "w");
-    char cmd_line[1024];
+    char cmd_line [1024];
 
     if (fp == NULL) {
         fprintf (stdout, "%s : couuld not create file for usblp test. ", __func__);
         return 0;
     }
-
+#if defined (USE_PROTOCOL_EPL)
+    fputs  (EPL_FORM_START, fp);
+    memset (cmd_line, 0, sizeof(cmd_line));
+    if (ch) sprintf(cmd_line, EPL_FORM_MAC_R, NLP_MODEL ? 0:10, NLP_MODEL ? 6:16, msg);
+    else    sprintf(cmd_line, EPL_FORM_MAC_L, NLP_MODEL ? 0:10, NLP_MODEL ? 6:16, msg);
+    fputs  (cmd_line, fp);
+    fputs  (EPL_FORM_END, fp);
+    fclose (fp);
+#else
     fputs  (ZPL_FORM_START, fp);
     memset (cmd_line, 0, sizeof(cmd_line));
     if (ch) sprintf(cmd_line, ZPL_FORM_MAC_R, msg);
@@ -273,8 +241,9 @@ int usblp_print_mac (char *msg, int ch)
     fputs  (cmd_line, fp);
     fputs  (ZPL_FORM_END, fp);
     fclose (fp);
+#endif
 
-    memset (cmd_line, 0x00, sizeof(cmd_line));
+    memset  (cmd_line, 0x00, sizeof(cmd_line));
     sprintf (cmd_line, "%s", "lpr usblp_mac.txt -P zebra 2<&1");
 
     if ((fp = popen(cmd_line, "w")) != NULL) {
@@ -287,13 +256,25 @@ int usblp_print_mac (char *msg, int ch)
 int usblp_print_err (const char *msg1, const char *msg2, const char *msg3, int ch)
 {
     FILE *fp = fopen ("usblp_err.txt", "w");
-    char cmd_line[1024];
+    char cmd_line [1024];
 
     if (fp == NULL) {
         fprintf (stdout, "%s : couuld not create file for usblp test. ", __func__);
         return 0;
     }
 
+#if defined (USE_PROTOCOL_EPL)
+    fputs  (EPL_FORM_START, fp);
+    memset (cmd_line, 0, sizeof(cmd_line));
+    sprintf(cmd_line, EPL_FORM_ERR_1, ch ? '>' : '<', NLP_MODEL ? 0:4, msg1);
+    fputs  (cmd_line, fp);
+    sprintf(cmd_line, EPL_FORM_ERR_2, ch ? '>' : '<', NLP_MODEL ? 0:4, msg2);
+    fputs  (cmd_line, fp);
+    sprintf(cmd_line, EPL_FORM_ERR_3, ch ? '>' : '<', NLP_MODEL ? 0:4, msg3);
+    fputs  (cmd_line, fp);
+    fputs  (EPL_FORM_END, fp);
+    fclose (fp);
+#else
     fputs  (ZPL_FORM_START, fp);
     memset (cmd_line, 0, sizeof(cmd_line));
     sprintf(cmd_line, ZPL_FORM_ERR_1, ch ? '>' : '<', msg1);
@@ -304,8 +285,9 @@ int usblp_print_err (const char *msg1, const char *msg2, const char *msg3, int c
     fputs  (cmd_line, fp);
     fputs  (ZPL_FORM_END, fp);
     fclose (fp);
+#endif
 
-    memset (cmd_line, 0x00, sizeof(cmd_line));
+    memset  (cmd_line, 0x00, sizeof(cmd_line));
     sprintf (cmd_line, "%s", "lpr usblp_err.txt -P zebra 2<&1");
 
     if ((fp = popen(cmd_line, "w")) != NULL) {
@@ -315,6 +297,29 @@ int usblp_print_err (const char *msg1, const char *msg2, const char *msg3, int c
 }
 
 //------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+int usblp_zpl_init (void)
+{
+    FILE *fp = fopen ("usblp_init.txt", "w");
+    char cmd_line [1024];
+
+    if (fp == NULL) {
+        fprintf (stdout, "%s : couuld not create file for usblp test. ", __func__);
+        return 0;
+    }
+
+    fputs  (ZPL_USB_INIT, fp);
+    fclose (fp);
+
+    memset  (cmd_line, 0x00, sizeof(cmd_line));
+    sprintf (cmd_line, "%s", "lpr usblp_err.txt -P zebra 2<&1");
+
+    if ((fp = popen(cmd_line, "w")) != NULL) {
+        pclose(fp);
+    }
+    return 1;
+}
+
 //------------------------------------------------------------------------------
 int get_usblp_ptype (void); /* epl, zpl */
 
@@ -359,8 +364,14 @@ int usblp_config (void)
     }
 
     fprintf (stdout, "*** USB Label Printer setup is complete. ***\n");
-    fprintf (stdout, "*** Printer Device Name   : %s\n", usblp_device);
-//    fprintf (stdout, "*** Printer Protocol Type : %s\n", get_usblp_ptype() ? "ZPL(ZD230D)" : "EPL(GC420D)");
+    fprintf (stdout, "*** Printer Device Name : %s\n", usblp_device);
+    NLP_MODEL = (strstr (usblp_device, "ZPL") != NULL) ? 1 : 0;
+    fprintf (stdout, "*** Printer Device Type : %s\n",
+                            NLP_MODEL ? "ZPL(ZD230D)" : "EPL(GC420D)");
+
+#if !defined (USE_PROTOCOL_EPL)
+    usblp_zpl_init ();
+#endif
     return 1;
 }
 //------------------------------------------------------------------------------
